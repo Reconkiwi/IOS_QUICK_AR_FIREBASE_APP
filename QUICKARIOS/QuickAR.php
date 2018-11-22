a00185a5a2b71fc0a71ad797f1f155e0a2f345ce
reconkiwi <beny.goodwin@gmail.com>
2018-11-22 02:28:49 +0000

    Initial Commit

---
+++ QUICKARIOS/QuickAR.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>AR Library</title>
+</head>
+
+<body>
+    <a href="models/house1.usdz" rel="ar">
+        <img src="images/house1.png">
+    </a>
+
+</body>
+
+</html
